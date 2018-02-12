cba71745e3db2be028d4976ae5fab2b36e15dac1
khurramsaeed <5n8BX6dp>
2018-02-12 14:49:50 +0100

    Ingredient model class created

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,3 @@
 export class Recipe {
-  public name: string;
-  public descr: string;
-  public imageUrl: string;
-
-  constructor(name: string, descr: string, imageUrl: string) {
-    this.name = name;
-    this.descr = descr;
-    this.imageUrl = imageUrl;
-  }
+  constructor(public name: string, public descr: string, public imageUrl: string) {}
 }
